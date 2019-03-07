63ce9f069436e4b9c098d23c5b75f630ea5ed659
Xavier Cadena <xacadena@gmail.com>
2019-03-07 12:12:22 -0600

    Update macaddr-fuzz.cpp

--- macaddr-fuzz.cpp
+++ macaddr-fuzz.cpp
@@ -3,7 +3,7 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
 
-	folly::StringPiece bin1(data, size)
+	folly::StringPiece bin1(data, size);
         folly::MacAddress::fromBinary(bin1);
         return 0;
 }
